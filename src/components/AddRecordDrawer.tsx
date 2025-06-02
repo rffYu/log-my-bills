ea61809f1701dd0b9f91993fa283ed18abc544c7
dr <dream11203@live.com>
2025-06-02 19:27:48 +0800

    PICK FROM DEMO0

--- src/components/AddRecordDrawer.tsx
+++ src/components/AddRecordDrawer.tsx
@@ -18,9 +18,18 @@ const typeReverseMap = {
   '1': '收入',
 };
 
+const startDate = new Date('2025-05-01');
+const endDate = new Date('2025-06-01');
+
+const dummyDateRange = [];
+for (let d = new Date(startDate); d <= endDate; d.setDate(d.getDate() + 1)) {
+  dummyDateRange.push(d.toISOString().slice(0, 10));
+}
+
 const AddRecordDrawer = ({ visible, onClose, onSubmit }: Props) => {
   const [amount, setAmount] = useState('');
   const [title, setTitle] = useState('');
+  const [date, setDate] = useState(new Date().toISOString().slice(0,10));
   const [type, setType] = useState(null);
   const [categoryId, setCategoryId] = useState(1); // example default
 
@@ -32,15 +41,26 @@ const AddRecordDrawer = ({ visible, onClose, onSubmit }: Props) => {
         <Text>添加记录</Text>
         <Input value={title} onInput={e => setTitle(e.detail.value)} placeholder="标题" />
         <Input type="number" value={amount} onInput={e => setAmount(e.detail.value)} placeholder="金额" />
-        <Picker
-          mode="selector"
-          range={ typeOptions }
-          onChange={e => {
-            const selected = typeOptions[Number(e.detail.value)];
-            setType(typeValueMap[selected]);
-        }}>
-          <View>{ type == null ? '选择类型' : typeReverseMap[type] }</View>
-        </Picker>
+        <View className="date-pricker">
+          <Picker mode="selector" range={ dummyDateRange } onChange={e => {
+            const selected = dummyDateRange[Number(e.detail.value)];
+            setDate(selected);
+          }}>
+            <Text>选择日期：{ date }</Text>
+          </Picker>
+        </View>
+
+        <View className="type-picker">
+          <Picker
+            mode="selector"
+            range={ typeOptions }
+            onChange={e => {
+              const selected = typeOptions[Number(e.detail.value)];
+              setType(typeValueMap[selected]);
+          }}>
+            <View>{ type == null ? '选择类型' : typeReverseMap[type] }</View>
+          </Picker>
+        </View>
 
         <Picker mode="selector" range={['吃饭', '交通']} onChange={e => setCategoryId(Number(e.detail.value))}>
           <Text>选择分类</Text>
